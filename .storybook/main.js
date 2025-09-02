d1ee1872777fcb28c0f3025bef5816845009eaed
AP <ayushpandeyb@gmail.com>
2025-09-02 22:33:18 +0530

    chore: upgrade Storybook to v9 and fix addon versions

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,9 @@
 export default {
-  framework: '@storybook/react-vite',
-  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  framework: "@storybook/react-vite",
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    '@storybook/addon-a11y',
-    '@storybook/addon-docs',
-    '@storybook/addon-vitest'
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
+    "@storybook/addon-vitest"
   ],
 };
