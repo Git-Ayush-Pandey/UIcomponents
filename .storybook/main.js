ba4b62a52bed05582a059cf1e914c339ed4e1f10
AP <ayushpandeyb@gmail.com>
2025-09-02 22:53:09 +0530

    chore: cleanup storybook config and addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,10 +2,6 @@ export default {
   framework: "@storybook/react-vite",
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    "@storybook/addon-a11y",
-    "@storybook/addon-docs",
-    "@storybook/addon-controls",
-    "@storybook/addon-actions",
-    "@storybook/addon-backgrounds",
+    "@storybook/addon-a11y"  // Only include this if you still need accessibility support
   ],
 };
